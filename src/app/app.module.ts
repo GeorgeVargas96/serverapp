91636b9450cb73e9a2cadfe43ecca96c43fbcb52
Vargas George Alexandru <george.vargas@e-uvt.ro>
2022-03-30 11:32:35 +0300

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NotificationModule } from './notification.module';
+
 
 @NgModule({
   declarations: [
@@ -9,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    NotificationModule
+    
 
   ],
   providers: [],
